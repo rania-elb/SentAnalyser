c6ac522194fc6ea403a62487b481d4bc1147765d
martinlvlle <martinsla@live.fr>
2018-12-17 11:17:33 +0100

    confusion matrix

--- SentAnalysisRNN.py
+++ SentAnalysisRNN.py
@@ -1,136 +1,229 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('train.tsv', sep='\t')
 
+df = df [:3000]
+
+train, test = train_test_split(df, test_size=0.20)
+
+
+# In[2]:
+
+
 from sklearn.feature_extraction.text import CountVectorizer
-
-df = df [:1000] #pour une charge de travail moindre
-vectorizer = CountVectorizer(binary=True)
-text_data = df["Phrase"]
-text_data = vectorizer.fit_transform(text_data)
-# text_data devient un vecteur creux n.m : n phrases, m mots dans le vocabulaire.
-
 import torch
 
-text_tensor = torch.from_numpy(text_data.todense()).float()
-# text_tensor est une matrice n.m
+vectorizer = CountVectorizer(binary=True)
+text_data = train["Phrase"]
+text_data = vectorizer.fit_transform(text_data)
+
+text_tensor_train = torch.from_numpy(text_data.todense()).float()
+
+text_tensor_train = text_tensor_train.unsqueeze(1)
 
 #ajout du cas où on ne connait pas le mot
-voca = vectorizer.get_feature_names() + list(['<unk>'])
+voca = vectorizer.get_feature_names() #+ list(['<unk>'])
 #ajout d'un zéro à la fin de chaque tensor
-unk = torch.zeros([text_tensor.shape[0],1], dtype=torch.float)
-text_tensor = torch.cat((text_tensor, unk), 1)
-#text_tensor passe à n.(m+1)
+# unk = torch.zeros([text_tensor_train.shape[0],1], dtype=torch.float)
+# text_tensor_train = torch.cat((text_tensor_train, unk), 1).unsqueeze(1)
 
-label_data = pd.get_dummies(df["Sentiment"])
-# get_dummies : donne une matrice n.c où c est le nombre de valeurs différentes pour Sentiment, soit 5 (de 0 à 4)
+def labelToVec(label):
+    # label_one_hot_vector = torch.tensor(pd.get_dummies(df["Sentiment"]).values)
+    label_tensor = torch.tensor(label["Sentiment"].values)
+    return label_tensor
 
-label_tensor = torch.tensor(label_data.values, dtype=torch.long)
-# contient la matrice de label_data
+label_tensor_train = labelToVec(train)
+
+
+# In[3]:
+
+
+from nltk import word_tokenize
+# nltk.download('punkt')
+
+def vocaToUnk(sentence, voca):
+    s = []
+    for w in word_tokenize(sentence):
+        if(w not in voca):
+            w = '<unk>'
+        s.append(w)
+    return " ".join(s)         
+                
+# text_test = test.Phrase.apply(lambda x : vocaToUnk(x, voca))
+#Ne prend pas en compte <unk>
+text_test = vectorizer.transform(test["Phrase"])
+
+text_tensor_test = torch.from_numpy(text_test.todense()).float()
+label_tensor_test = labelToVec(test)
+
+text_tensor_test = text_tensor_test.unsqueeze(1)
+
+
+# In[4]:
+
+
+print(text_tensor_test[1], text_tensor_train[0])
+
+
+# In[5]:
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# cuda object
+
+
+# In[6]:
+
 
 import torch.nn as nn
 
 class RNN(nn.Module):
-	def __init__(self, input_size, hidden_size, output_size):
-		super(RNN, self).__init__()
-		
-		self.hidden_size = hidden_size
-		
-		self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-		self.i2o = nn.Linear(input_size + hidden_size, output_size)
-		self.softmax = nn.LogSoftmax(dim=1)
-	
-	def forward(self, input, hidden):
-		combined = torch.cat((input.view(1, len(voca)), hidden), 1)
-		hidden = self.i2h(combined)
-		output = self.i2o(combined)
-		output = self.softmax(output)
-		return output, hidden
-	
-	def initHidden(self):
-		return torch.zeros(1, self.hidden_size, dtype=torch.float)
+    def __init__(self, input_size, embedding_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        
+        self.hidden_size = hidden_size
+        
+        self.embedding = nn.Embedding(input_size, embedding_size)
+
+        self.rnn = nn.RNN(input_size = input_size, hidden_size = hidden_size, batch_first = True)
+        self.h2o = nn.Linear(hidden_size, output_size)       
+        
+    def forward(self, input, hidden):
+        
+        hidden = hidden.to(device)
+        output, hidden = self.rnn(input, hidden)
+        output = self.h2o(hidden)
+        
+        return output, hidden
+
+    def initHidden(self, batch_size):
+        return torch.zeros(1, batch_size, self.hidden_size, dtype=torch.float)
 
 
-input_dim = len(voca) #nombre de mots, dimension de départ ; compte le mot inconnu <unk>
-hidden_dim = 100 # arbitraire ?
-output_dim = 5 #une dimension par sentiment
-
-rnn = RNN(input_dim, hidden_dim, output_dim)
-
-# hidden = torch.zeros(1000, hidden_dim, dtype=torch.float)
-
-# output, next_hidden = rnn(text_tensor, hidden)
-
-criterion = nn.NLLLoss()
-
-learning_rate = 0.005 #unité ?
-
-def train(category_tensor, line_tensor):
-	hidden = rnn.initHidden() #créée une matrice nulle
-	
-	rnn.zero_grad()
-	
-	#print(rnn.parameters()[0].grad)
-	#     print(line_tensor.size(), hidden.size())
-	#     for i in range(line_tensor.size()[0]):    
-	#         print(line_tensor[i].size())
-	output, hidden = rnn(line_tensor, hidden)
-	
-	loss = criterion(output, torch.tensor([category_tensor]))
-	loss.backward()
-	
-	# Add parameters' gradients to their values, multiplied by learning rate
-	for p in rnn.parameters():
-		print(type(p.grad))
-		p.add_(-learning_rate, p.grad.data)
-	
-	return output, loss.item()
-
-current_loss = 0
-
-########
-# Randomization des états initiaux
+# In[7]:
 
 
-# Find letter index from voca, e.g. "a" = 0
-def letterToIndex(letter):
-    return .index(letter)
+input_dim = len(voca)
+embedding_size = 100
+hidden_dim = 100
+output_dim = 5
+
+rnn = RNN(input_dim, embedding_size, hidden_dim, output_dim)
+
+import torch.optim as optim
+
+optimizer = optim.SGD(rnn.parameters(), lr=1e-3)
+
+def trainRNN(train_category, train_text, test_category, test_text, num_epoch, batch_size):
+    
+    train_category = train_category.to(device)
+    train_text = train_text.to(device)
+    test_category = test_category.to(device)
+    test_text = test_text.to(device)
+    
+    size_train = train_text.size(0)
+    size_test = test_text.size(0)
+    
+    hidden = rnn.initHidden(batch_size)   
+    criterion = nn.CrossEntropyLoss()    
+    rnn.zero_grad()
+    
+    total_pred = torch.tensor([], dtype = torch.long)
+    total_targ = torch.tensor([], dtype = torch.long)
+    
+    for epoch in range(num_epoch):
+        #un jour je ferais un truc propre
+        nb_batch_train = len(train_text) / batch_size
+        nb_batch_test = len(test_text) / batch_size
+        train_loss = 0
+        train_acc = 0
+        i = 0
+        while (i + batch_size) <= size_train:
+            tmp = i
+            i += batch_size
+            input = train_text[tmp:i]
+            target = train_category[tmp:i]
+            output, hidden = rnn(input, hidden)
+            
+            loss = criterion(output.squeeze(0), target)
+            loss.backward(retain_graph=True)
+            optimizer.step() 
+            
+            predicted = torch.argmax(output.data, dim=2)
+            correct = (predicted == target).sum().item()
+            
+            train_loss += loss.item()
+            train_acc += correct / batch_size
+        
+        i = 0
+        test_loss = 0
+        test_acc = 0        
+        
+        #j'aime faire des boucles presque pareilles
+        while (i + batch_size) <= size_test:
+            tmp = i
+            i += batch_size
+            input = test_text[tmp:i]
+            target = test_category[tmp:i]
+            
+            output, _ = rnn(input, hidden)
+            loss = criterion(output.squeeze(0), target)   
+            
+            predicted = torch.argmax(output.data, dim=2)
+            correct = (predicted == target).sum().item()
+            
+            test_loss += loss.item()
+            test_acc += correct / batch_size
+            
+            #dernière epoch
+            if epoch + 1 == num_epoch:
+                total_pred = torch.cat((total_pred, predicted), 1)
+                total_targ = torch.cat((total_targ, target), 0)
+                
+        print(epoch, "loss :", train_loss / nb_batch_train, "/ acc :", train_acc / nb_batch_train)
+        print("Test loss :", test_loss / nb_batch_test, "/ acc :", test_acc / nb_batch_test)
+        
+    print('Fini !')
+    
+    return total_pred, total_targ
 
 
-# Turn a line into a <line_length x 1 x n_letters>,
-# or an array of one-hot letter vectors
-def lineToTensor(line):
-    tensor = torch.zeros(len(line), 1, input_dim)
-    for li, letter in enumerate(line):
-        tensor[li][0][letterToIndex(letter)] = 1
-    return tensor
+# In[8]:
 
-import random
 
-all_categories = [0,1,2,3,4]
-#category_lines = {0:['null', 'horrible'], 1:['idiotic', 'negative'], 2 : ['normal'], 3 : ['cool'], 4: ['super'] }
+batch_size = 50
+nb_epoch = 5
 
-def randomChoice(l):
-    return l[random.randint(0, len(l) - 1)]
+pred, real = trainRNN(label_tensor_train, text_tensor_train, label_tensor_test, text_tensor_test, nb_epoch, batch_size)
 
-def randomTrainingExample():
-    category = randomChoice(all_categories)
-    line = randomChoice(category_lines[category])
-    category_tensor = torch.tensor([all_categories.index(category)], dtype=torch.long)
-    line_tensor = lineToTensor(line)
-    return category, line, category_tensor, line_tensor
 
-for i in range(10):
-    category, line, category_tensor, line_tensor = randomTrainingExample()
-    print('category =', category, '/ line =', line)
+# In[9]:
 
-######
 
-# for label, text in zip(label_tensor, text_tensor):
-for i in range(text_tensor.size()[0]):
-	output, loss = train(category_tensor, line_tensor)
-	current_loss += loss
+from sklearn.metrics import confusion_matrix
+from pandas_ml import ConfusionMatrix
+
+print(train.groupby('Sentiment')["Sentiment"].count())
+print(test.groupby('Sentiment')["Sentiment"].count())
+
+#warning chiante, mais np
+cm = ConfusionMatrix(real.data, pred.squeeze(0).data)
+confusion_matrix(real, pred.squeeze(0))
+
+
+# In[10]:
+
+
+get_ipython().magic('matplotlib inline')
+
+import matplotlib.pyplot as plt
+
+cm.plot()
+plt.show()
 
